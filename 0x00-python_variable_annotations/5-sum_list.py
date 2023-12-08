1d2f839fd295de48854f416dda7e78230af48681
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-12-08 16:53:30 +0300

    import List from typing module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+from typing import List
 """ a type annotated function that takes list of float"""
 """and return their sums as float"""
 
 
-def sum_list(input_list: [float]) -> float:
-    sum = 0
+def sum_list(input_list: List[float]) -> float:
+    list_sum = 0
     for i in input_list:
-        sum += i
-    return sum
-# print(sum_list([1.0, 2.0, 3.0]))
+        list_sum += i
+    return list_sum
