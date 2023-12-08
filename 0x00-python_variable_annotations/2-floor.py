ee689d6e488cedca62ec0680f6ed2fa262744459
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-12-08 16:04:52 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+"""Take float type and return floor value"""
+
+
+def floor(n: float) -> int:
+    """return floor of float type int"""
+    return math.floor(n)
