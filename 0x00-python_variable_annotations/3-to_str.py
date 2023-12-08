b0cc0532db9b6c24a46c8fd649226a7f6ee9cf14
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-12-08 16:17:52 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" take a string and return string repr of the string"""
+
+
+def to_str(n: str) -> str:
+    """return str type from type from str arg"""
+    return str(n)
