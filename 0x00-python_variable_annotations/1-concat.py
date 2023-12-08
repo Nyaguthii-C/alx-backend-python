6010e33e9917a6ffe5d15ff55f632696939a86a1
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-12-08 15:46:17 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+"""take string type arguments and return concatenated string"""
+def concat(str1: str, str2: str) -> str:
+    """concatenate string types and return a string"""
+    return str1 + str2
