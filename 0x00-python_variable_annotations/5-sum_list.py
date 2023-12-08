2770ac47fa0c875b6a30717634aa870334a58ac3
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-12-08 16:55:43 +0300

    initialize sum value with float

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,7 +5,7 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    list_sum = 0
+    list_sum = 0.0
     for i in input_list:
         list_sum += i
     return list_sum
