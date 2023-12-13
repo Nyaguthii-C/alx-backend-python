aa2c9c8a6e07aca90172c72d59cd2df922289a63
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-12-13 12:43:08 +0300

    define async function, wait then return value

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basics of async"""
+# import modules
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """wait for a random number of sec and return the number of sec waited"""
+    i: float = random.uniform(0, max_delay)
+
+    await asyncio.sleep(i)
+
+    return i
