810f1d0d52c075d2a2d410185a7ce07583ab9bf0
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-12-17 01:27:05 +0300

    add type hinting for generator fn

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[int, None, None]:
     """loop 10 times, wait asynchronously then yield a random number"""
     for _ in range(10):
         await asyncio.sleep(1)
